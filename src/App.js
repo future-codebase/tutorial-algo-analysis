2bd1c9c00d40b95eb87be33ef1df04b31be02f49
unknown <mhdaman60@gmail.com>
2023-09-11 22:19:10 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Aman</h1>
-    </div>
+    <>
+    <Navbar heading="Subject"/>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar(props) {
+  return (
+    <div>
+      <h1>Hello, {props.heading}!</h1>
+    </div>
+  );
+}
+
+export default Navbar;
